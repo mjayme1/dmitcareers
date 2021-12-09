914b9f87619f5a48e0be4bed287a7dc41401a0b5
Marjorie Ann Jayme <mjayme1@nait.ca>
2021-12-09 11:04:12 -0700

    website update

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+        Smooth scroll functionality for anchor links (animates the scroll
+        rather than a sudden jump in the page)
+        */
+$('.js-anchor-link').click(function(e){
+    e.preventDefault();
+    var target = $($(this).attr('href'));
+    if(target.length){
+        var scrollTo = target.offset().top;
+        $('body, html').animate({scrollTop: scrollTo+'px'}, 800);
+    }
+    });
